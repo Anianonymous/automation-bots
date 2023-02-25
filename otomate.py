f939765fe4a9441bf4ff41182c820a9dc10460a5
Anirudh Hegde <105560839+Anianonymous@users.noreply.github.com>
2023-02-25 09:30:29 +0530

    Update otomate.py

--- otomate.py
+++ otomate.py
@@ -5,25 +5,17 @@ base="dark"
 name=st.text_input("enter your name: ")
 # choose your choice
 choice=st.radio("select your required websites:",("Github","Blogger"))
-
+fin=""
 # create a function
 def result():
     if choice=="Github":
-        wb.open("https://github.com/Anianonymous/automation-bots")
+        fin="https://github.com/Anianonymous/automation-bots"
     elif choice=="Blogger":
-        wb.open("https://anoanymousphotos.blogspot.com/")
+        fin="https://anoanymousphotos.blogspot.com/"
     else:
-        pass
-
+        fin="undefined"
+    st.success(fin)
 if st.button("result"):
     # call the function
     result()
-# import streamlit as st
-# base="dark"
 
-# st.write("---")
-# n1=st.number_input(label="enter your first number: ")
-# def fin():
-#     st.success(n1)
-# if st.button('final'):
-#     fin()
